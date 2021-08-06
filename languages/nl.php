a171b13bd7a0ffae8a314b8452ef840abd6e79e3
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-08-06 10:01:14 +0200

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file was created by Translation Editor v8.0
+ * On 2021-08-06 10:00
+ */
+
+return array (
+  'fontawesome:settings:href' => 'CDN href',
+  'fontawesome:settings:integrity' => 'Integrity key',
+  'fontawesome:settings:zip' => 'Upload ZIP bestand',
+  'fontawesome:settings:zip:help' => 'Hier kun je de download van de font awesome website https://fontawesome.com/download uploaden. Dit webfont zal dan lokaal worden geserveerd.',
+  'fontawesome:settings:remove_zip' => 'Verwijder geüpload font',
+  'fontawesome:settings:save:zip_error' => 'Het geüploade zip bestand kan niet worden uitgepakt',
+);
